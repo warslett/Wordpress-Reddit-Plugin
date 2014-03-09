03e51bc3d413d51c326a63864cc96541650da3b4
William Arslett <wia2@aber.ac.uk>
2014-03-09 13:28:22 +0000

    Load document object model from Reddit

--- wpredditfeed.php
+++ wpredditfeed.php
@@ -1,28 +1,31 @@
 <?php
 
 /*
-Plugin Name: Wordpress Reddit Feed
-Description: The Wordpress Reddit Feed loads popular content from Reddit.
-Version: 1.0
-Author: William Arslett
-Author URI: http://www.williamarslett.com
-*/
+  Plugin Name: Wordpress Reddit Feed
+  Description: The Wordpress Reddit Feed loads popular content from Reddit.
+  Version: 1.0
+  Author: William Arslett
+  Author URI: http://www.williamarslett.com
+ */
 
 //SETUP
-function super_plugin_install(){
+function super_plugin_install() {
     //Do some installation work
 }
 
-register_activation_hook(__FILE__,'super_plugin_install');
+register_activation_hook(__FILE__, 'super_plugin_install');
 
-function feed_insert($content)
-{
-  if (preg_match('/\{REDDIT*.+\}/',$content))
-    {
-      $feed_output = "Testing Testing";
-      $content = preg_replace('/\{REDDIT*.+\}/',$feed_output,$content);
+function feed_insert($content) {
+    if (preg_match('/\{REDDIT*.+\}/', $content)) {
+        $resp = wp_remote_get('http://www.reddit.com');
+        $dom = new DOMDocument;
+        libxml_use_internal_errors(true);
+        $dom->loadHTML($resp['body']);
+        libxml_use_internal_errors(false);
+        $feed_output = "Testing Testing";
+        $content = preg_replace('/\{REDDIT*.+\}/', $feed_output, $content);
     }
-  return $content;
+    return $content;
 }
 
-add_filter('the_content','feed_insert');
+add_filter('the_content', 'feed_insert');
