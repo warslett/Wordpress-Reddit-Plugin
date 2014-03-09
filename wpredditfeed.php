3e0e4c05438702f7aa2222a8506c99ad97812ec5
William Arslett <wia2@aber.ac.uk>
2014-03-09 22:13:32 +0000

    bit of formatting

--- wpredditfeed.php
+++ wpredditfeed.php
@@ -20,7 +20,10 @@ register_activation_hook(__FILE__, 'super_plugin_install');
 function feed_insert($content) {
     if (preg_match('/\[redditlatest*.+\]/', $content)) {
         $feed = new RedditFeed;
-        $content = preg_replace('/\[redditlatest*.+\]/', $feed->getHeadlines(), $content);
+        $content = preg_replace(
+                        '/\[redditlatest*.+\]/', 
+                        $feed->getHeadlines(), 
+                        $content);
     }
     return $content;
 }
